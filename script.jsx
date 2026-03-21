b5a350d89df0b1a12c6c67077aa0b6ff92826b1c
Tushar shivade <tusharshivade122@gmail.com>
2026-03-21 15:23:04 +0530

    react js floder done

---
+++ script.jsx
@@ -0,0 +1,19 @@
+// var h1=document.createElement('h1')
+// h1.innerHTML='Hello form js'
+// document.body.appendChild(h1)   
+
+// import a from './app.js'
+// console.log(a)
+
+//jsx  it is comninaton of javascript and html
+//virtual dom and real dom diffference
+// var h1=<h1>Hello from jsx</h1>
+
+// function App(){
+//     console.log("Hello from App component");
+// }
+// <App/>
+
+
+//empty tags in react
+//fragemnits
---
+++ tushar/src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+
+  return (
+    <>
+    <div id="parent" >
+      <h1 id="child1">hello tushar</h1>
+      <h1 id="child2">hello shantunu</h1>
+    </div>
+
+      <div>  <h1 id="child3">hello mayur</h1>
+   </div>
+     
+    </>
+    
+    
+  )
+}
+export default App
